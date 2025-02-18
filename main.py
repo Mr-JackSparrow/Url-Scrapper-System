e869122f370bd11669b83838ebd5c2984fd62d2a
Mr-JackSparrow <132438113+Mr-JackSparrow@users.noreply.github.com>
2025-02-19 00:57:56 +0530

    removed global main

--- main.py
+++
@@ -1,8 +0,0 @@
-from upstash_redis import Redis
-
-redis = Redis(url="https://intimate-lacewing-16779.upstash.io", token="AUGLAAIjcDFiMzU1YjgyNWQ4Yjk0ZjhhYjUxZDlhZTRlYmQyMTI3NXAxMA")
-
-redis.set("foo", "bar")
-value = redis.get("foo")
-
-print(value)
