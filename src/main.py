974d0116345b9d7b11e4139c70244c223cd8544a
Mr-JackSparrow <132438113+Mr-JackSparrow@users.noreply.github.com>
2025-02-19 01:23:07 +0530

    unused removes

--- src/main.py
+++ src/main.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI
 from src.api.routes.routes import router as auth_router
 
-# Create the FastAPI app
 app = FastAPI()
 
-print("Auth Service is running")
-
-# Include the router
 app.include_router(auth_router, tags=["auth"])
 
 @app.get("/")
 def read_root():
-    print("shubya bhikarchod")
-    return {"message": "Auth Service"}
+    return {"message": "Welcome to Url Scraper"}
