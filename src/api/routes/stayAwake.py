852c7e2c8d0ac64b8477341518498c8b742c2044
Mr-JackSparrow <132438113+Mr-JackSparrow@users.noreply.github.com>
2025-02-19 15:29:56 +0530

    silly mistake

--- src/api/routes/stayAwake.py
+++ src/api/routes/stayAwake.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Header, HTTPException
 awakeRouter = APIRouter()
 
 
-@awakeRouter.get("/stay-awake")
+@awakeRouter.get("/stay-awake", include_in_schema=False)
 async def stayAwake(header : str = Header(None)):
     if header != "devOnly":
         raise HTTPException(status_code=403, detail="Forbidden: Invalid Header")
